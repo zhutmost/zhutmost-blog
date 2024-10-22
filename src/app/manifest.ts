2c52ba162d928bb5a691bdcbe1b85e1536f2658f
zhutmost <zhutmost@outlook.com>
2024-10-22 15:45:25 +0800

    style: fix double-quote lint bugs

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,15 +12,15 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#fff',
     icons: [
       {
-        "src": "/favicons/android-chrome-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png"
+        src: '/favicons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
       {
-        "src": "/favicons/android-chrome-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png"
-      }
+        src: '/favicons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
     ],
   }
 }
