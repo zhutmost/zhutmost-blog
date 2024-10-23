db23ff41dcab549e5b740ce9ed26524496630e17
zhutmost <zhutmost@outlook.com>
2024-10-23 23:51:10 +0800

    fix: remove archive from sitemap to optimize SEO

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,20 +3,20 @@ import siteConfig from '@/lib/site-config'
 import allPostsSorted from '@/lib/post-sort'
 import { allAuthors } from '@/content-collections'
 
-type SiteMap = MetadataRoute.Sitemap
-type SiteMapSingleFile = MetadataRoute.Sitemap[number]
+type Sitemap = MetadataRoute.Sitemap
+type SitemapSingleFile = MetadataRoute.Sitemap[number]
 
-export default function sitemap(): SiteMap {
+export default function sitemap(): Sitemap {
   const siteUrl = siteConfig.siteUrl
 
-  const homeRoute: SiteMapSingleFile = {
+  const homeRoute: SitemapSingleFile = {
     url: siteUrl,
     lastModified: new Date().toISOString(),
     priority: 1,
     changeFrequency: 'monthly',
   }
 
-  const postRoutes: SiteMap = allPostsSorted
+  const postRoutes: Sitemap = allPostsSorted
     .filter((post) => !post.draft)
     .map((post) => ({
       url: `${siteUrl}/posts/${post.slugPath}`,
@@ -24,16 +24,16 @@ export default function sitemap(): SiteMap {
       changeFrequency: 'monthly',
     }))
 
-  const authorRoutes: SiteMap = allAuthors.map((author) => ({
+  const authorRoutes: Sitemap = allAuthors.map((author) => ({
     url: `${siteUrl}/about/${author.slugPath}`,
     lastModified: author.dateUpdate.toISOString(),
     priority: 0.5,
     changeFrequency: 'monthly',
   }))
 
-  const pages = ['news', 'archive', 'tags']
+  const pages = ['news', 'tags']
 
-  const pageRoutes: SiteMap = pages.map((route) => ({
+  const pageRoutes: Sitemap = pages.map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString(),
     priority: 0.5,
