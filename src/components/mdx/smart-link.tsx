1da2a1a94df83677f1db45d406620f2d84baba67
zhutmost <zhutmost@outlook.com>
2024-11-02 12:20:44 +0800

    feat: add an icon for external links in posts

--- src/components/mdx/smart-link.tsx
+++ src/components/mdx/smart-link.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import NextLink from 'next/link'
 import { cn } from '@/lib/utils'
+import { IconExternalLink } from '@tabler/icons-react'
 
 export default function SmartLink({
   href,
   className,
+  children,
   ...rest
 }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   const isInternalPage = href && href.startsWith('/')
@@ -14,20 +16,31 @@ export default function SmartLink({
     'break-words text-primary no-underline hover:text-primary/80 hover:underline hover:underline-offset-2'
 
   if (isInternalPage) {
-    return <NextLink className={cn(inlineLinkStyles, className)} href={href} {...rest} />
+    return (
+      <NextLink className={cn(inlineLinkStyles, className)} href={href} {...rest}>
+        {children}
+      </NextLink>
+    )
   }
 
   if (isInternalAnchor) {
-    return <a className={cn(inlineLinkStyles, className)} href={href} {...rest} />
+    return (
+      <a className={cn(inlineLinkStyles, className)} href={href} {...rest}>
+        {children}
+      </a>
+    )
   }
 
   return (
     <a
-      className={cn(inlineLinkStyles, className)}
+      className={cn('inline-flex items-center', inlineLinkStyles, className)}
       target="_blank"
       rel="noopener noreferrer"
       href={href}
       {...rest}
-    />
+    >
+      {children}
+      <IconExternalLink className="ml-1 inline h-[1em] w-[1em]" />
+    </a>
   )
 }
