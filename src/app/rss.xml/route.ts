b0ef6e7f540375bc8c06b3e510b2caa707b1f787
zhutmost <zhutmost@outlook.com>
2024-11-06 01:56:59 +0800

    fix: stop RSS2 feed logging in consoles

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -2,7 +2,6 @@ import generateRssFeed from '@/lib/rss'
 
 export async function GET() {
   const feed = generateRssFeed()
-  console.log(feed.rss2())
   return new Response(feed.rss2(), {
     headers: {
       'Content-Type': 'text/xml',
