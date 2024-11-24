166f57a08bf29414b0c2084beec99ea2a4f868aa
zhutmost <zhutmost@outlook.com>
2024-11-25 00:30:46 +0900

    feat: change the style of timeline news

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -20,54 +20,52 @@ export interface TimelineProps {
 
 function TimelineItem({ date, title, description, icons }: TimelineItemProps) {
   return (
-    <li className="mb-3 ms-4">
-      <div className="absolute -start-1.5 mt-2 h-3 w-3 rounded-full border border-background bg-border" />
-      <time className="mb-1 text-sm font-normal leading-none text-muted-foreground">
-        {date.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}
-      </time>
-      <h3 className="text-base font-semibold text-foreground">{title}</h3>
-      {description && (
-        <p className="mb-1 text-sm font-normal text-muted-foreground">{description}</p>
-      )}
+    <div className="group relative py-2 pl-8 sm:pl-32">
+      <div className="mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-accent before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-4 after:border-muted after:bg-primary group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-[6.5rem] sm:after:left-0 sm:after:ml-[6.5rem]">
+        <time className="left-0 mb-3 inline-flex h-6 w-20 translate-y-0.5 items-center justify-center rounded-full bg-secondary text-xs font-semibold uppercase text-secondary-foreground sm:absolute sm:mb-0">
+          {date.toLocaleDateString('en-US', { month: 'short', year: 'numeric' })}
+        </time>
+        <div className="text-base font-bold text-foreground">{title}</div>
+      </div>
+      {description && <div className="text-sm text-muted-foreground">{description}</div>}
       {icons && (
-        <div className="flex gap-2">
+        <div className="flex gap-2 py-1">
           {Object.entries(icons).map(([key, item]) => (
             <SocialIcon key={key} name={key} icon={item.icon} href={item.href} size={6} />
           ))}
         </div>
       )}
-    </li>
+    </div>
   )
 }
 
 export default function Timeline({ timelineNews, findMore = false }: TimelineProps) {
   if (timelineNews.length === 0) {
     return (
-      <ol className="relative ml-5 border-s border-border md:ml-10">
+      <div className="-my-6">
         <TimelineItem
           date={new Date()}
           title={'Nothing happened yet'}
           description={'Always believe that something wonderful is about to happen.'}
         />
-      </ol>
+      </div>
     )
   }
 
   return (
-    <ol className="relative ml-5 border-s border-border md:ml-10">
+    <div className="-my-6">
       {timelineNews.map((news, index) => (
         <TimelineItem {...news} key={index} />
       ))}
-      <li className="mb-6 ms-4">
-        <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-background bg-border" />
-        {findMore ? (
-          <NextLink href={'/news'} className={buttonVariants({ variant: 'outline' })}>
-            Find more <IconArrowRight className={'ml-2 h-5 w-5'} />
-          </NextLink>
-        ) : (
-          <p className="mb-2 text-base font-normal text-muted-foreground">That&apos;s all folks!</p>
-        )}
-      </li>
-    </ol>
+      {findMore && (
+        <div className="group relative pb-6 pl-8 pt-2 sm:pl-32">
+          <div className="mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-accent before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-4 after:border-muted after:bg-primary group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-[6.5rem] sm:after:left-0 sm:after:ml-[6.5rem]">
+            <NextLink href="/news" className={buttonVariants({ variant: 'outline' })}>
+              Find more <IconArrowRight className="ml-2 h-5 w-5" />
+            </NextLink>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
