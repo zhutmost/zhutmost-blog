373664a71e1fac360742535b5507511011f8c205
zhutmost <github-action@actions-template-sync.noreply.github.com>
2025-03-01 01:49:33 +0000

    chore(template): merge template changes :up:
    Signed-off-by: zhutmost <github-action@actions-template-sync.noreply.github.com>

--- data/timeline-news.ts
+++ data/timeline-news.ts
@@ -1,7 +1,48 @@
 import { TimelineItemProps } from '@/components/timeline'
-import TimelineNews2025 from '@/data/news/timeline-news-2025'
-import TimelineNews2024 from '@/data/news/timeline-news-2024'
 
-const timelineNews: TimelineItemProps[] = [...TimelineNews2025, ...TimelineNews2024]
+const timelineNews: TimelineItemProps[] = [
+  {
+    date: new Date('2023-07-01'),
+    title: 'Copy is All You Need — published on arXiv',
+    description:
+      'Atrae aliquid arbor, saecli litore medulla; ait candentia. Avellit conubia, Iunonis es, victoria modo, habet chrysolithi adsensu potuisse in Ecce arva. Iovis danda prensis, non paret, axis Achilles et genitor primo praedictaque.',
+    icons: {
+      PDF: { icon: 'IconFileTypePdf', href: 'https://arxiv.org/pdf/2307.06962v1' },
+      arXiv: { icon: 'IconLink', href: 'https://arxiv.org/abs/2307.06962v1' },
+    },
+  },
+  {
+    date: new Date('2023-04-16'),
+    title: 'Retention Is All You Need — published on arXiv',
+    description:
+      'Cum domos nullum minitantiaque habili. Anno petit te est. Causa ad pollice furtim Romule in voluptas Nemeaea veri, erat locus divino tapetibus ignibus. Aut curvo certe venter ligno ille foliis ut toro spatiosa tulit.',
+  },
+  {
+    date: new Date('2021-11-10'),
+    title: 'Gradients are Not All You Need — published on arXiv',
+    description:
+      'Nocti vidi est: concita potentia Aiaci domo fer. Moratur neve sit arbore corpora inritans aspicis arboribus errat! Erat non parantem terra parata relicta ille?',
+  },
+  {
+    date: new Date('2020-06-13'),
+    title: '15 Keypoints Is All You Need — accepted by CVPR 2020',
+    description:
+      'Aquae haec fragorem, non tua tonitruque manu, ad distamus, inde si videri et quique dicitur? Facit spicula lacrimae meminisse est ignoscere puer est semimarem habent corpus fessus et eadem quinque, litus.',
+  },
+  {
+    date: new Date('2017-12-01'),
+    title: 'Attention is All You Needed — accepted by NIPS 2017',
+    description:
+      'Ipse grave vale, capit clipeo inponit citus quis quaque adpellare non, spectasse portus tegitur Laiades. Agmen ubi dixit et defuit artem: est: manum domus, proposita.',
+    icons: {
+      PDF: { icon: 'IconFileTypePdf', href: 'https://arxiv.org/pdf/1706.03762v7' },
+      arXiv: { icon: 'IconLink', href: 'https://arxiv.org/abs/1706.03762v7' },
+      GitHub: {
+        icon: 'IconBrandGithub',
+        href: 'https://github.com/jadore801120/attention-is-all-you-need-pytorch',
+      },
+    },
+  },
+]
 
 export default timelineNews
